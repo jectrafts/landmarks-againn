fe92bf55ac80cccd241b8945b0e436c0c16d5330
jectrafts <chris.joe.m2@gmail.com>
2024-11-20 23:21:28 +0530

    Add files via upload

---
+++ hold.py
@@ -0,0 +1,58 @@
+if closey >= 0.01 or closey >= 0.01:
+    print('close')
+    hand_state == 0
+
+else:
+    print('open')
+    hand_state == 1
+
+
+hi(tilt right)
+
+five_landmark = hand_landmarks.landmark[4]
+x5 = five_landmark.x
+y5 = five_landmark.y
+z5 = five_landmark.z
+# print("x =", x5, ", y =", y5,)
+seven_landmark = hand_landmarks.landmark[3]
+x8 = seven_landmark.x
+y8 = seven_landmark.y
+z8 = five_landmark.z
+# print("x =", x7, ", y =", y7,)
+y11 = y8 * 7
+
+if y8 <= y5:
+    print('hi')
+
+i
+
+five_landmark = hand_landmarks.landmark[8]
+x5 = five_landmark.x
+y5 = five_landmark.y
+z5 = five_landmark.z
+# print("x =", x5, ", y =", y5,)
+seven_landmark = hand_landmarks.landmark[5]
+x8 = seven_landmark.x
+y8 = seven_landmark.y
+z8 = seven_landmark.z
+# print("x =", x7, ", y =", y7,)
+
+if z5 >= z8 * 0.85:
+    print('i')
+
+you
+
+five_landmark = hand_landmarks.landmark[5]
+x5 = five_landmark.x
+y5 = five_landmark.y
+z5 = five_landmark.z
+# print("x =", x5, ", y =", y5,)
+seven_landmark = hand_landmarks.landmark[8]
+x8 = seven_landmark.x
+y8 = seven_landmark.y
+z8 = seven_landmark.z
+# print("x =", x7, ", y =", y7,)
+
+if z8 * -1 >= z5 * -1.3 and z8 * -1 >= z5 * -0.001:
+    print('u')
+
